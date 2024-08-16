05b8946f18be4c8edbefafc17301bb4bb065a0db
Saahil Basak <saahilbasak@thecuriouscat.local>
2024-08-16 20:24:11 +0530

    reevaluated code

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 const express = require('express');
+const getRoutes = require('./routes/getRoutes')
 const app = express();
-const getRoutes = require('./routes/getRoutes');
-// const ngrok = require('ngrok');
-
-const PORT = 3001;
-
-//middlewares
+const ngrok = require('ngrok');
+const PORT = process.env.PORT || 3000;
+//middleware
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1', getRoutes);
+//routes
+app.use('/', getRoutes);
 
-
-// Global error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Server error:', err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  });
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`started at ${PORT}`);      
 });
-
-
-// (async function() {
-//     const url = await ngrok.connect({
-//         proto: 'http',
-//         addr: PORT,
-//         authtoken: ""
-//     });
-//     console.log(url);
-// })();
+//connecting ngrok 
+//you can signup to ngrok website and get your own authToken
+(async function(){
+    const url = await ngrok.connect({
+        proto:'http',
+        addr:PORT,
+        authtoken:'2ItcO1pxPHwKl81EJuLodWGgOVi_5sd5GCq3BQbR1a2jJuFr3'
+    });
+    console.log(url);
+})();
--- scrapeFn/scrape.js
+++ scrapeFn/scrape.js
@@ -1,61 +1,56 @@
-const puppeteer = require('puppeteer');
+
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 const data = {
-    list: []
-};
+    list : []
+}
 
+async function main (skill) {
+    const browser = await puppeteer.launch({headless:false})
+    const page = await browser.newPage()
+    await page.goto(`https://in.indeed.com/m/jobs?q=${skill}&l=`, {
+        timeout:0,
+        waitUntil:'networkidle0',
+    })
+    //  Get a screenshot of the page
+    // await page.screenshot({ path: 'example.png', fullPage: true });
 
-async function main(skill) {
-    
-    const browser = await puppeteer.launch({headless: false })                      //launches cromium 
-    const page = await browser.newPage()                                                 //open new tab
-    await page.goto(`https://in.indeed.com/jobs?q=${skill}&l=Bangalore%2C+Karnataka`,  {  //go to page
-        timeout: 0,
-        waitUntil: 'networkidle0'
-    });
-    console.log("Inside browser");
-
-
-    await page.waitForSelector('td.resultContent');
-
+     //  Get a PDF of the page
+    // await page.pdf({ path: 'example.pdf', format: 'A4' });
 
     const jobData = await page.evaluate(async (data) => {
-        const items = document.querySelectorAll('td.resultContent');
-
-        console.log("Website data got");
+        const items = document.querySelectorAll('td.resultContent')
         items.forEach((item, index) => {
             console.log(`scraping data of product: ${index}`)
-            const title = item.querySelector('h2.jobTitle > a')?.innerText;
-            const link = item.querySelector('h2.jobTitle > a')?.href;
-            let salary = item.querySelector('div.metadata.salary-snippet-container > div')?.innerText;
-            const companyName = item.querySelector('span.companyName')?.innerText;
-
-            //edge cases
-            if(salary == null){
-                salary = "not defined"
+            const title= item.querySelector('h2.jobTitle>a') && item.querySelector('h2.jobTitle>a').innerText
+            const link =item.querySelector('h2.jobTitle>a') && item.querySelector('h2.jobTitle>a').href
+            let salary= item.querySelector('div.metadata.salary-snippet-container > div') && item.querySelector('div.metadata.salary-snippet-container > div').innerText
+            const companyName = item.querySelector('span.companyName') && item.querySelector('span.companyName').innerText
+            if(salary===null){
+                salary="not defined"
             }
-
             data.list.push({
-                title: title,
-                salary: salary,
-                companyName: companyName,
-                link: link
+                title:title,
+                salary:salary,
+                companyName:companyName,
+                link:link
             })
-        }, data);
+        })
         return data;
-    });
+        }, data)
 
-    console.log(`successfully collected ${jobData.list.length} products`)
+        console.log(`successfully collected ${jobData.list.length} products`)
+       
+        let response = await jobData;
+        //convert from an object to string and making json file and writing data to it
+        let json = await JSON.stringify(jobData,null,2)
+        fs.writeFile('jobs.json', json, 'utf8', () => {
+            console.log('succesfully written data')
+        });
 
-    let response = await jobData;
-    let json = await JSON.stringify(jobData, null, 2);
-    fs.writeFile('job.json', json, 'utf-8', () => {
-        console.log('written in job.json');
-    });
-
-    browser.close();
-    return response;
-};
-
+        browser.close();
+        
+        return response;
+}
 
 module.exports = main;
